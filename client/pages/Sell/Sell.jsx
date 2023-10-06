1b29aab73c61f1e36ff2aa8baf501e2c4ee5e90f
Dwijesek <dwijesek@gmail.com>
2023-10-05 19:22:09 -0700

    test commit

---
+++ client/pages/Sell/Sell.jsx
@@ -0,0 +1,9 @@
+import Item from './components/Item'
+
+const Sell = () => {
+  return(
+  <div className="sell">
+   <Item/>
+  </div>);
+}
+export default Sell;
---
+++ client/pages/Sell/components/Item.jsx
@@ -0,0 +1,19 @@
+const Item = () => {
+
+  return (
+    <div className="item">
+      <img src="" alt="Image of an item for sale" />
+    <div className="sell-details">
+      <h2>Product Name</h2>
+      <h2>Product Price</h2>
+      <h4>Seller Name</h4>
+      <h4>Seller Location</h4>
+    </div>
+    <div className="sell-product-details">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+    </div>
+  );
+}
+ 
+export default Item;
