e209e82f27e3463d4c1b9e2e8db45eeb713636bb
Dwijesek <dwijesek@gmail.com>
2023-10-07 14:00:31 -0700

    setup fetching and most of the page

--- client/pages/Sell/components/Item.jsx
+++ client/pages/Sell/components/Item.jsx
@@ -1,16 +1,17 @@
-const Item = () => {
 
+const Item = ({item}) => {
+  
   return (
     <div className="item">
       <img src="" alt="Image of an item for sale" />
     <div className="sell-details">
-      <h2>Product Name</h2>
-      <h2>Product Price</h2>
-      <h4>Seller Name</h4>
-      <h4>Seller Location</h4>
+      <h2>{item.name}</h2>
+      <h2>{item.price}</h2>
+      <h4>{item.sellerName}</h4>
+      <h4>{item.sellerLocation}</h4>
     </div>
     <div className="sell-product-details">
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <p>{item.details}</p>
     </div>
     </div>
   );
