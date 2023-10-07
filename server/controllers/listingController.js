0a05c90604447a2d68d1f2ac35fde7691cf497ff
Albert Hu <albhu24@gmail.com>
2023-10-07 13:49:11 -0700

    listingRouter structure finished

---
+++ server/controllers/listingController.js
@@ -0,0 +1,12 @@
+const listingController = {};
+
+listingController.getAllListings = (req, res, next) => {};
+
+listingController.getListing = (req, res, next) => {};
+
+listingController.createListing = (req, res, next) => {};
+
+listingController.updateListing = (req, res, next) => {};
+
+listingController.deleteListing = (req, res, next) => {};
+module.exports = listingController;
