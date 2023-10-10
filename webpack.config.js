8e7b34a7ec2e4f5de14ae754b6b9e9684ea1dfa8
Dwijesek <dwijesek@gmail.com>
2023-10-09 20:19:02 -0700

    sell, item details
    webpack is running. sell and item details done. Will make the adjustments after exact details confirmed

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,7 @@ module.exports = {
             title: 'Development',
             template: './client/index.html'
         }),
-        new webpack.DefinePlugin(envKeys)
+        
     ],
     devServer: {
         static: {
@@ -49,7 +49,7 @@ module.exports = {
             directory: path.join(__dirname, '/dist')
         },
         proxy: {
-            '/': 'http://localhost:3000',
+            '/api': 'http://localhost:4000',
         },
         hot: true,
         historyApiFallback: true
