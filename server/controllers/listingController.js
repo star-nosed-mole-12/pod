39172679b8bb1ff1007e4507eb2462528f56b6a5
Daniel Song <81377675+djsong15@users.noreply.github.com>
2023-10-09 18:14:17 -0700

    Complete createListing middleware route; requires db query params to pull required data from req.body

--- server/controllers/listingController.js
+++ server/controllers/listingController.js
@@ -78,9 +78,41 @@ listingController.getListing = async (req, res, next) => {
     }
 };
 
-listingController.createListing = (req, res, next) => {};
+listingController.createListing = async (req, res, next) => {
+    const client = await pool.connect()
+        .catch(err => next({
+            log: `listingController - pool connection failed ERROR: ${err}`,
+            message: {
+                err: 'Error in listingController.createListing. Check server logs'
+            }
+        }));
+    try {
+        const createListingQuery = `INSERT INTO listings
+                (product_name, price, quantity, category, seller_id)
+            VALUES ($1, $2, $3, $4, $5)`;
+        console.log(req.body);
 
-listingController.updateListing = (req, res, next) => {};
+        await client.query(createListingQuery, [
+            req.body.name,
+            req.body.price,
+            req.body.qty,
+            req.body.category,
+            req.body.sellerId
+        ]);
+    } catch (err) {
+        return next({
+            log: `listingController.createListing - inserting into listings table ERROR: ${err}`,
+            message: {
+                err: 'Error in listingController.createListing. Check server logs'
+            }
+        });
+    } finally {
+        client.release();
+        return next();
+    }
+};
 
-listingController.deleteListing = (req, res, next) => {};
+listingController.updateListing = async (req, res, next) => {};
+
+listingController.deleteListing = async (req, res, next) => {};
 module.exports = listingController;
